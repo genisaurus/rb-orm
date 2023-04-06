6a63f298bba62b93736611c0cd7f9df5607119f5
genisaurus <rusty.barron@gmail.com>
2023-04-05 22:59:13 -0400

    forgot to add new files

---
+++ src/test/java/com/rborm/session/SessionUpdateTest.java
@@ -0,0 +1,117 @@
+package com.rborm.session;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.rborm.test.model.Account;
+import com.rborm.test.model.Apartment;
+import com.rborm.test.model.Resident;
+
+public class SessionUpdateTest {
+	
+	private static SessionFactory sf;
+	private static Session session;
+	private static Connection conn;
+	
+	@BeforeAll
+	public static void configure() throws SQLException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		try {
+			Class.forName("org.h2.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		conn = DriverManager.getConnection("jdbc:h2:mem:internal", "sa", "sa");
+		sf = new SessionFactory();
+		session = sf.newSession();
+
+		String createAccounts =
+				"CREATE TEMP TABLE IF NOT EXISTS accounts ("
+				+ "account_id INT NOT NULL, "
+				+ "username VARCHAR(50) NOT NULL,"
+				+ "PRIMARY KEY(account_id) ); ";
+		String createApartments = 
+				"CREATE TEMP TABLE IF NOT EXISTS apartments ("
+				+ "room INT NOT NULL,"
+				+ "condition VARCHAR(50) NOT NULL,"
+				+ "PRIMARY KEY(room) );";
+		String createResidents =
+				"CREATE TEMP TABLE IF NOT EXISTS residents ("
+				+ "uuid UUID NOT NULL,"
+				+ "name VARCHAR(50) NOT NULL,"
+				+ "apt INT,"
+				+ "FOREIGN KEY(apt) REFERENCES apartments(room),"
+				+ "PRIMARY KEY(uuid) );";
+		
+		conn.createStatement().execute(createAccounts);
+		conn.createStatement().execute(createApartments);
+		conn.createStatement().execute(createResidents);
+	}
+	
+	@AfterEach
+	public void clearTables() throws SQLException {
+		conn.createStatement().execute("DELETE FROM accounts");
+		conn.createStatement().execute("DELETE FROM residents");
+		conn.createStatement().execute("DELETE FROM apartments");
+	}
+	
+	@Test
+	public void testSaveSingleObjectNoTx() throws SQLException {
+		conn.createStatement().execute("INSERT INTO accounts VALUES(1, 'test_user')");
+		Account test = new Account(1, "updated_test_user");
+		session.update(test);
+		ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM accounts WHERE account_id=1");
+		rs.next();
+		assertEquals(rs.getString(2), "updated_test_user");
+	}
+	
+	@Test 
+	public void testSaveSingleObjectTx() throws SQLException {
+		conn.createStatement().execute("INSERT INTO accounts VALUES(1, 'test_user')");
+		Account test = new Account(1, "updated_test_user");
+		Transaction tx = session.beginTransaction();
+		session.update(test);
+		tx.commit();
+		ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM accounts WHERE account_id=1");
+		rs.next();
+		assertEquals(rs.getString(2), "updated_test_user");
+	}
+	
+	@Test
+	public void testSaveCompositeObjectNoTx() throws SQLException {
+		conn.createStatement().execute("INSERT INTO apartments VALUES(101, 'poor')");
+		conn.createStatement().execute("INSERT INTO residents VALUES(X'7000c33195884cceaeec56eefc10dca2', 'Jane Doe', 101)");
+		Apartment apt = new Apartment(101, "actually pretty nice");
+		UUID uuid = new UUID( 0x7000c33195884ccel, 0xaeec56eefc10dca2l);
+		Resident res = new Resident(uuid, "Jane Doe", apt);
+		session.update(res);
+		ResultSet rs1 = conn.createStatement().executeQuery("SELECT * FROM apartments WHERE room=101");
+		rs1.next();
+		assertEquals(rs1.getString(2), "actually pretty nice");
+	}
+	
+	@Test
+	public void testSaveCompositeObjectTx() throws SQLException {
+		conn.createStatement().execute("INSERT INTO apartments VALUES(101, 'poor')");
+		conn.createStatement().execute("INSERT INTO residents VALUES(X'7000c33195884cceaeec56eefc10dca2', 'Jane Doe', 101)");
+		Apartment apt = new Apartment(101, "actually pretty nice");
+		UUID uuid = new UUID( 0x7000c33195884ccel, 0xaeec56eefc10dca2l);
+		Resident res = new Resident(uuid, "Jane Doe", apt);
+		Transaction tx = session.beginTransaction();
+		session.update(res);
+		tx.commit();
+		ResultSet rs1 = conn.createStatement().executeQuery("SELECT * FROM apartments WHERE room=101");
+		rs1.next();
+		assertEquals(rs1.getString(2), "actually pretty nice");
+	}
+
+}
